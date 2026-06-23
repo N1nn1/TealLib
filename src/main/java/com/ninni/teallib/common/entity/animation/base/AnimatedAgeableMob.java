b45f7515c608f6c962e88e6889f759147785614e
Gaetano/Ninni <64173061+N1nn1@users.noreply.github.com>
2026-06-23 23:36:56 +0200

    more bases

---
+++ src/main/java/com/ninni/teallib/common/entity/animation/base/AnimatedAgeableMob.java
@@ -0,0 +1,50 @@
+package com.ninni.teallib.common.entity.animation.base;
+
+import com.ninni.teallib.common.entity.animation.EntityAnimationController;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class AnimatedAgeableMob extends AgeableMob {
+    private static final EntityDataAccessor<CompoundTag> ANIMATION_SYNC = SynchedEntityData.defineId(AnimatedAgeableMob.class, EntityDataSerializers.COMPOUND_TAG);
+    protected final EntityAnimationController animations = new EntityAnimationController(this);
+
+    protected AnimatedAgeableMob(EntityType<? extends AgeableMob> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    protected void defineSynchedData(SynchedEntityData.Builder builder) {
+        super.defineSynchedData(builder);
+        builder.define(ANIMATION_SYNC, new CompoundTag());
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (level().isClientSide) {
+            animations.tickClient();
+        } else {
+            animations.tickServer();
+            if (animations.consumeDirty()) entityData.set(ANIMATION_SYNC, animations.writeSync());
+        }
+    }
+
+    @Override
+    public void onSyncedDataUpdated(@NotNull EntityDataAccessor<?> key) {
+        super.onSyncedDataUpdated(key);
+        if (ANIMATION_SYNC.equals(key) && level().isClientSide) {
+            animations.readSync(entityData.get(ANIMATION_SYNC));
+        }
+    }
+
+    public EntityAnimationController animations() {
+        return animations;
+    }
+}
